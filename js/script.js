7b1277ad4c0014f54e1ef11fc127f4a7b534f14f
andrew.maach <andrew.maach@gmail.com>
2014-03-04 21:11:23 -0600

    completed jquery version of graph.

--- js/script.js
+++ js/script.js
@@ -9,17 +9,12 @@ $(function() {
 	load_chart();
 });
 
+
 function load_chart() {
-	var chart = document.getElementById("jobs-chart");
-	var jobs = chart.getElementsByTagName("li");
-	var view_width = window.getComputedStyle(chart).width.replace("px", "").replace("PX", "");
-	for(var i = 0; i < jobs.length; ++i) {
-	   var job = jobs[i];
-	   var wage = job.getElementsByTagName("span")[0].innerHTML;
-	   var amount = wage.replace("$","").replace(",","");
-	   var new_size = (amount * view_width) / 80000;
-	   console.log(amount +"*"+ view_width + "/ 80000");
-	   job.style.width = new_size + "PX";
-	}
+	$("#jobs ul li").each(function(){
+		var view_width = $("#jobs-chart").width();
+		var amount = $(this).find("span").text().replace("$","").replace(",","");
+		var new_size = (amount * view_width) / 80000;
+		$(this).width(new_size);
+	});
 }
-	        
