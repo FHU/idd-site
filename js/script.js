e18aa4b989c8fc6d48c6757c67b818c85bde1750
samtatum88 <sam.tatum@students.fhu.edu>
2014-03-18 17:28:46 -0500

    Scroll spy for navigation bar

--- js/script.js
+++ js/script.js
@@ -184,3 +184,35 @@ var end_2_lines = end2.group(end2.line(0, 1, 800, 1), end2.line(7, 1, 7, 30))
 end_2_lines.attr({ stroke: "#000", strokeWidth: .5});
 end2.circle(7, 30, 7).attr({fill: "#67c4ef"});
 /*****************************************************************************************/
+
+/**************************Scrollspy****************************************/
+var lastId,
+    topMenu = $("#navigation-scrollspy"),
+    topMenuHeight = topMenu.outerHeight()+15,
+    menuItems = topMenu.find("a"),
+    scrollItems = menuItems.map(function(){
+      var item = $($(this).attr("href"));
+      if (item.length) { return item; }
+    });
+$(window).scroll(function(){
+   var fromTop = $(this).scrollTop()+topMenuHeight;
+   var cur = scrollItems.map(function(){
+     if ($(this).offset().top < fromTop)
+       return this;
+   });
+   cur = cur[cur.length-1];
+   var id = cur && cur.length ? cur[0].id : "";  
+   if (lastId !== id) {
+       lastId = id;
+       menuItems
+         .parent().removeClass("active")
+         .end().filter("[href=#"+id+"]").parent().addClass("active");
+   }                   
+});
+
+/*****************************************************************************************/
+
+
+
+
+
