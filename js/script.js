1b1eddbeaa87a9ee0b238972a1246fc53a42a9ae
Joey Fiddler <joseph.fiddler@students.fhu.edu>
2014-03-20 19:29:48 -0500

    Changessss

--- js/script.js
+++ js/script.js
@@ -7,33 +7,6 @@ $(document).ready(function(){
 	$('header .container #img-text h3').css('letter-spacing', '13px');
 });
 
-$(document).ready(function() {
-
-    $("#learn-notecards").owlCarousel({
-
-        //navigation : true, // Show next and prev buttons
-        slideSpeed : 300,
-        paginationSpeed : 400,
-        singleItem:true
-     
-      // "singleItem:true" is a shortcut for:
-      // items : 1, 
-      // itemsDesktop : false,
-      // itemsDesktopSmall : false,
-      // itemsTablet: false,
-      // itemsMobile : false
-     
-    });
-
- 	$(".left-arrow").click({
- 		
- 	});
-
- 	$(".right-arrow").click({
- 		
- 	});
-});
-
 var has_graph_been_reached = false;
 $(document).ready(function() {
 	controller = new ScrollMagic();
