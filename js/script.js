05313f1c3c62a44c5dd8220fb00474c8ebbcbc75
Mark Roberts <mark.roberts1@students.fhu.edu>
2014-04-24 17:11:21 -0500

    infinite scroll on carousel

--- js/script.js
+++ js/script.js
@@ -8,23 +8,12 @@ $(document).ready(function(){
 	$('header .container #img-text h2').css('letter-spacing', '7px');
 });
 
-$(document).ready(function() {
-
-    $("#learn-notecards").owlCarousel({
-
-        //navigation : true, // Show next and prev buttons
-        slideSpeed : 300,
-        paginationSpeed : 400,
-        singleItem:true
-     
-      // "singleItem:true" is a shortcut for:
-      // items : 1, 
-      // itemsDesktop : false,
-      // itemsDesktopSmall : false,
-      // itemsTablet: false,
-      // itemsMobile : false
-     
-    });
+$(document).ready(function(){
+	$('.carousel slide').slick({
+	  autoplay: true,
+	  arrows: false
+	});
+	$('.carousel slide').slickPlay();
 });
 
 var has_graph_been_reached = false;
