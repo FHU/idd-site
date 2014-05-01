94bdc414c6959ecead5173e515fbb0dad55e35fa
LukeFader <lukefader@Lukes-MacBook-Air.local>
2014-05-01 17:34:05 -0500

    offcanvas.js change to collapse mobile nav

--- js/offcanvas.js
+++ js/offcanvas.js
@@ -1,5 +1,10 @@
 $(document).ready(function () {
   $('[data-toggle=offcanvas]').click(function () {
-    $('.row-offcanvas').toggleClass('active')
+    $('.row-offcanvas').toggleClass('active');
+  });
+  
+  //added by luke to collapse the mobile nav when an anchor is clicked
+  $("header nav ul li a").click(function(){
+    $('.row-offcanvas').toggleClass('active');
   });
 });
